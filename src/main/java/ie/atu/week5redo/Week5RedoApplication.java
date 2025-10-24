2bf8c4f9dea8ae2ac4288a652f475ffe36f3b4c5
meike <g00407095@atu.ie>
2025-10-24 22:58:28 +0100

    init commit w/ added dependencies

---
+++ src/main/java/ie/atu/week5redo/Week5RedoApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5redo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5RedoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5RedoApplication.class, args);
+    }
+
+}
