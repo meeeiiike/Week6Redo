d830d5cfc5bac8156ca4f9793f998c66ec0e5d11
meike <g00407095@atu.ie>
2025-10-24 23:05:32 +0100

    created model class

--- src/main/java/ie/atu/week5redo/model/Passenger.java
+++ src/main/java/ie/atu/week5redo/model/Passenger.java
@@ -1,4 +1,19 @@
 package ie.atu.week5redo.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@AllArgsConstructor@NoArgsConstructor@Builder
 public class Passenger {
+    @NotBlank@Size(max = 40)
+    private String passengerId;
+    @NotBlank@Size(max = 100)
+    private String name;
+    @Email@NotBlank
+    private String email;
 }
